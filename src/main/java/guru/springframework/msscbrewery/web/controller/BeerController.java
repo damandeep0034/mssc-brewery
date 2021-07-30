a5030b5bfaeeaf481dbf024e056a8b86f663da7e
Damandeep Singh <damandeep0034@gmail.com>
2021-07-31 01:19:11 +0530

    Changes for Spring REST MVC

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,6 +26,32 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity addBeer(@RequestBody BeerDto beerDto)
+    {
+        System.out.println("In Post mapping");
+        BeerDto savedDto= beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("Location", "/api/v1/beer"+savedDto.getId().toString());
+
+        return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto){
+        beerService.updateBeer(beerId,beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable UUID beerId)
+    {
+        beerService.deleteBeer(beerId);
     }
 
 }
